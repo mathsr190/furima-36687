edadd7da6613cb617c212fcecf8539e2781dceef
mathsr190 <math.sr.190@gmail.com>
2021-09-25 15:25:01 +0900

    未ログインのときはログイン画面に遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,15 @@
 class ItemsController < ApplicationController
-
+  before_action :move_to_login, only: :new
   def index
   end
-  
+
   def new
+  end
+
+  private
+  def move_to_login
+    unless user_signed_in?
+      redirect_to authenticate_user!
+    end
   end
 end
