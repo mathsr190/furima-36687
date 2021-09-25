98c55fbea43adb07a0ebc0aae8fd042f84a08f85
mathsr190 <math.sr.190@gmail.com>
2021-09-25 16:46:20 +0900

    itemモデルにバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :price
+  end
   belongs_to :user
   has_one_attached :image
 end
