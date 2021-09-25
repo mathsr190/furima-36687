fb7267f572571a800652d8c6a1e95e40e60b4da8
mathsr190 <math.sr.190@gmail.com>
2021-09-25 16:18:16 +0900

    ActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,18 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @items = Item.new
+  end
+
+  def create
+
   end
 
   private
+  def item_params
+    params.require(:item).permit(:image,:name,:info,:category_id,:sales_status_id,:shipping_fee_status_id,:prefecture_id,:scheduled_delivery_id,:price).merge(user_id: current_user.id)
+  end
+
   def move_to_login
     unless user_signed_in?
       redirect_to authenticate_user!
