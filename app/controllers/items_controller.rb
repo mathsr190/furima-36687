3391cdbfada362ab5ac3a82578e64b2fd4f67be9
mathsr190 <math.sr.190@gmail.com>
2021-09-25 17:02:25 +0900

    new,createアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,11 +4,16 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
   def create
-
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
   end
 
   private
