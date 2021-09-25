8c35d13dc6790799df0ecf24555a773f6460ffca
mathsr190 <math.sr.190@gmail.com>
2021-09-25 17:46:05 +0900

    ActiveHashクラス作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,13 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :info
-    validates :category_id
-    validates :sales_status_id
-    validates :shipping_fee_status_id
-    validates :prefecture_id
-    validates :scheduled_delivery_id
+    with_options numericality: { other_than: 0 , message: "can't be blank"} do
+      validates :category_id
+      validates :sales_status_id
+      validates :shipping_fee_status_id
+      validates :prefecture_id
+      validates :scheduled_delivery_id
+    end
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is invalid'}
   end
   belongs_to :user
