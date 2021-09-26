39aba7c19ed0e243ce9137e7c03b46edb1d8982c
mathsr190 <math.sr.190@gmail.com>
2021-09-26 15:52:54 +0900

    controllerauthenticate_user!可読性向上

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_login, only: :new
+  before_action :authenticate_user!, only: :new
   def index
   end
 
@@ -21,9 +21,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id,
                                  :scheduled_delivery_id, :price).merge(user_id: current_user.id)
-  end
-
-  def move_to_login
-    redirect_to authenticate_user! unless user_signed_in?
   end
 end
