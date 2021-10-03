92b72f94c0ea1f6e02de1021f43126b4e603f380
mathsr190 <math.sr.190@gmail.com>
2021-10-03 18:03:38 +0900

    ordersコントローラーネスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'orders/index'
   devise_for :users
   root to: "items#index"
-  resources :items
-  resources :orders, only: :index
+  resources :items do
+    resources :orders, only: [:index]
+  end
 end
